f73f25613d3b556f9b1658b02a49a0d162df4a00
Matt Cadorette <mcadorette@gmail.com>
2018-11-01 08:45:07 -0400

    Initial commit from Atomist

---
+++ src/main/java/ipcrm1/Ipcrm1Application.java
@@ -0,0 +1,12 @@
+package ipcrm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipcrm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipcrm1Application.class, args);
+	}
+}
